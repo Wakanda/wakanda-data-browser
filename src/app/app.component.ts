51fa4c0c703c1e8e5abe857bef019990f97b8e2c
Hamza HIKMAT <hamzahik@gmail.com>
2018-02-27 16:59:55 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { WakandaClient } from 'wakanda-client/browser/no-promise';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('paginator') paginator;
+
+  title = 'Wakanda Data Browser';
+  client;
+  catalog;
+  dataclasses;
+  currentDataclass;
+  data;
+  columns = [];
+
+  pageSizeOptions = [20, 40, 80, 160];
+  length = 0;
+  start = 0;
+  pageSize = 60;
+
+  query = "";
+  lastQuery = "";
+
+  constructor() {
+    this.client = new WakandaClient({ host: 'http://localhost:8081' });
+    this.client.getCatalog().then(c => {
+      this.catalog = c;
+      this.dataclasses = Object.keys(c);
+      this.currentDataclass = this.dataclasses[0];
+      this.dataclasses.length && this.fetchData();
+    });
+  }
+
+  fetchData() {
+    let dataclass = this.catalog[this.currentDataclass];
+
+    if (this.lastQuery !== this.query) {
+      this.start = 0;
+      this.paginator.firstPage();
+    }
+
+    dataclass.query({
+      filter: this.query,
+      pageSize: this.pageSize,
+      start: this.start
+    })
+      .then(response => {
+        this.columns = dataclass.attributes.map(attr => attr.name);
+        this.data = new MatTableDataSource<Object>(response.entities);
+        this.pageSize = response._pageSize;
+        this.length = response._count;
+      });
+  }
+
+  handlePageEvent(event) {
+    //event = {pageIndex: 0, pageSize: 40, length: 1000}
+    this.start = event.pageSize * event.pageIndex;
+    this.pageSize = event.pageSize;
+
+    this.fetchData();
+  }
+
+  switchDataclass(dataclass) {
+    this.currentDataclass=dataclass;
+    this.fetchData();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatSidenavModule,
+  MatButtonModule,
+  MatListModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule
+} from '@angular/material';
 import { AppComponent } from './app.component';
 
+@NgModule({
+  exports: [
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule
+  ]
+})
+export class AngularMaterialModule {}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
