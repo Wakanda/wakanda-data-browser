42bef796dd29e95a1706dbabd730fca8707618a9
Hamza HIKMAT <hamzahik@gmail.com>
2018-03-05 16:26:20 +0100

    fix the scaffolding of data-table component tests

--- src/app/core/components/data-table/data-table.component.spec.ts
+++ src/app/core/components/data-table/data-table.component.spec.ts
@@ -1,6 +1,17 @@
+import { Component, Input } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { StoreModule, Store, combineReducers } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import * as fromRoot from '../../../reducers';
+import { MaterialModule } from '../../../material'
 
 import { DataTableComponent } from './data-table.component';
+
+@Component({ selector: 'app-cell', template: '' })
+class CellComponentStub { 
+  @Input() column;
+  @Input() value;
+}
 
 describe('DataTableComponent', () => {
   let component: DataTableComponent;
@@ -8,9 +19,14 @@ describe('DataTableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DataTableComponent ]
+      declarations: [DataTableComponent, CellComponentStub],
+      imports: [
+        BrowserAnimationsModule,
+        MaterialModule,
+        StoreModule.forRoot(fromRoot.reducers),
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
